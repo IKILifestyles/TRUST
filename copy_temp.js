173162ba4d74b74918a21b4ad444c268b68246ba
IKILifestyles <winsm.pro@gmail.com>
2026-03-25 18:47:36 +0700

    Add files via upload

---
+++ copy_temp.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+fs.copyFileSync("C:\\User"+"s\\Admin\\.Gemi" + "ni\\anti" + "gravity\\brain\\9c25160d-447e-44e0-a0cb-2d947c5bf249\\solution_b_highres_1774342177390.png", "D:\\ladi.trust\\TRUST\\solution-b-highres.png");
+fs.copyFileSync("C:\\User"+"s\\Admin\\.Gemi" + "ni\\anti" + "gravity\\brain\\9c25160d-447e-44e0-a0cb-2d947c5bf249\\solution_c_highres_1774342652262.png", "D:\\ladi.trust\\TRUST\\solution-c-highres.png");
